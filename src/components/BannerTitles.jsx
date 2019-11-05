75e1bae7805e4438429e11a1d462715740940338
SimplyGA <jonpak1987@gmail.com>
2019-11-05 11:33:29 -0800

    Randomized images on banner

--- src/components/BannerTitles.jsx
+++ src/components/BannerTitles.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Row, Carousel, Container } from "react-bootstrap";
 
-let slides = ["a.jpg", "b.jpg", "c.jpg"];
+function shuffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+}
+let slides = ["a.jpg", "b.jpg", "c.jpg", "d.jpg", "e.jpg", "f.jpg"];
+
 let slideimgPath = require.context("../../public/images/banner", true);
+
+let newSlides = shuffle(slides);
 
 const BannerTitles = props => {
 	return (
 		<Container fluid className="col-12 my-5">
-			<Carousel id="bannerslide" className="col-12 p-0" controls={false} indicators={false} pauseOnHover={false} slide={false} style={{ border: "2px solid black" }}>
-				{slides.map(pic => {
+			<Carousel id="bannerslide" className="col-12 p-0" interval={2500} controls={false} indicators={false} pauseOnHover={false} slide={false} style={{ border: "2px solid black" }}>
+				{newSlides.map(pic => {
 					let imgSrc = slideimgPath("./" + `${pic}`);
 					return (
 						<Carousel.Item className="bnslider-item">
